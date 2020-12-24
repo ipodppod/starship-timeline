7d41327021a6a80695ee103862d90e1a56b9446b
Moshe Levine <moses.levine@gmail.com>
2020-12-24 18:02:56 +0200

    better naming in data strcuture

--- generate.js
+++ generate.js
@@ -6,7 +6,7 @@ const engine = new liquid.Engine()
 const template = fs.readFileSync('./source/index.liquid').toString()
 const data = jsonfile.readFileSync('./data.json')
 
-data.timeline.forEach(event => {
+data.events.forEach(event => {
     event.date = new Date([event.year, event.month, event.day].join('-'))
 })
 
