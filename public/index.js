8d0407a7c4e4e8e683406d358ee030f1d7ed3510
Moshe Levine <moses.levine@gmail.com>
2020-12-28 23:00:47 +0200

    better performance when scrolling

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const timeline = document.querySelector('nav');
     const events = timeline.querySelectorAll('li');
-    const gallery = document.querySelector('.gallery')
+    const images = document.querySelectorAll('.gallery .event');
 
     document.body.style.height = (100 + events.length * 15) + 'vh';
     window.addEventListener('scroll', render);
@@ -12,14 +12,16 @@ document.addEventListener('DOMContentLoaded', function() {
         const progress = document.body.scrollTop / (document.body.scrollHeight - window.innerHeight);
         const index = Math.min(Math.floor((events.length) * progress), events.length-1);
         const internalProgress = ((events.length-1) * progress - index);
-        const event = events[index];
-        if (event.classList.contains('active') === false) {
-            const active = document.querySelector('nav li.active');
-            active && active.classList.remove('active');
-            event.classList.add('active')
-        }
-        gallery.style.top = - index * 100 + 'vh';
+        singleOut(events, index, 'active');
+        singleOut(images, index, 'active');
         timeline.style.top = -(timeline.offsetHeight - window.innerHeight) * progress + 'px';
+    }
+
+    function singleOut(collection, index, cls) {
+        if (collection[index].classList.contains(cls) === false) {
+            collection.forEach(element => element.classList.remove('active'));
+            collection[index].classList.add('active')
+        }
     }
 
 })
