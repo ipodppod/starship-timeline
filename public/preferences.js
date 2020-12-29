72996f6190d2cff4e5f17747f71f60f5cfc5d0c5
Moshe Levine <moses.levine@gmail.com>
2020-12-30 00:24:19 +0200

    toggleable preferences

--- public/preferences.js
+++ public/preferences.js
@@ -4,6 +4,15 @@
         time_direction: 'forward'
     }
 
+    document.addEventListener('DOMContentLoaded', function(e) {
+        document.querySelector('main menu li[name="preferences"]').addEventListener('click', function() {
+            document.body.classList.add('show-preferences');
+        })
+        document.querySelector('main menu li[name="close"]').addEventListener('click', function() {
+            document.body.classList.remove('show-preferences');
+        })
+    })
+
     document.addEventListener('buttonpressed', function(e) {
         if (e.detail.buttonset.getAttribute('name') == 'time-direction' && e.detail.pressed) {
             preferences.time_direction = e.detail.button.name;
@@ -20,7 +29,7 @@
         const engine = new window.liquidjs.Liquid();
         engine.parseAndRender(template, context)
             .then(html => {
-                document.querySelector('nav .timeline').innerHTML = html;
+                document.querySelector('main nav').innerHTML = html;
                 window.scrolling.restart();
             });
     }
