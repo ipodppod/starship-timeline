1bc1eacaea2722cbd5c5f91461371058a6ca2a8a
Moshe Levine <moses.levine@gmail.com>
2020-12-29 11:02:18 +0200

    some improvement to scrolling performance

--- public/index.js
+++ public/index.js
@@ -4,23 +4,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const events = timeline.querySelectorAll('li');
     const images = document.querySelectorAll('.gallery .event');
 
-    document.body.style.height = (100 + events.length * 15) + 'vh';
     window.addEventListener('scroll', render);
     render();
 
     function render() {
         const progress = document.body.scrollTop / (document.body.scrollHeight - window.innerHeight);
         const index = Math.min(Math.floor((events.length) * progress), events.length-1);
-        const internalProgress = ((events.length-1) * progress - index);
-        singleOut(events, index, 'active');
-        singleOut(images, index, 'active');
-        timeline.style.top = -(timeline.offsetHeight - window.innerHeight) * progress + 'px';
+        if (index != this.index) {
+            markSingle(events, index, 'active');
+            throttle(50, () => markSingle(images, index, 'active'));
+            this.index = index;
+        }
     }
 
-    function singleOut(collection, index, cls) {
-        if (collection[index].classList.contains(cls) === false) {
-            collection.forEach(element => element.classList.remove('active'));
-            collection[index].classList.add('active')
+    function markSingle(collection, index, cls) {
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].classList[i == index ? 'add' : 'remove'](cls);
+        }
+    }
+
+    function throttle(delay, fn) {
+        this.lastCall = this.lastCall || 0;
+        const now = Date.now();
+        const timePassed = (now - this.lastCall);
+        if (timePassed >= delay) {
+            this.lastCall = now;
+            fn();
+        } else {
+            const wait = (delay - timePassed);
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => {
+                this.lastCall = now;
+                fn();
+            }, wait);
         }
     }
 
